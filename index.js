12f1043a56aaf3ac536529877d6488a3c4384e89
m-ichelleliu <michelleliu@princeton.edu>
2021-10-15 23:14:24 -0400

    actual

--- index.js
+++ index.js
@@ -3,55 +3,124 @@ const client = AgoraRTC.createClient({
     mode: "rtc",
 });
 
-var personCount = 0;
-console.log("hi");
+var mic_muted = false;
+var vid_muted = false;
+var sharing_screen = false;
 
+var localTracks = {
+    videoTrack: null,
+    audioTrack: null
+};
+
+// Create Client
 $("#hi").submit(async function (e) {
+    client.on("user-published", publishUser);
+    client.on("user-left", leaveUser);
+
     e.preventDefault();
-    console.log("b");
-    const uid = await client.join("fd08b9bee9554c789692cd2477b977ea", $("#room").val(), null);
-    console.log("Joined");
+    await client.join("fd08b9bee9554c789692cd2477b977ea", $("#room").val(), null);
+
+    // Adjust button status
+    $("#leave").attr("disabled", false);
+    $("#join").attr("disabled", true);
+    $("#mic").attr("disabled", false);
+    $("#vid").attr("disabled", false);
+    $("#screen").attr("disabled", false);
     
     // Create audio and video tracks
-    const cameraTrack = await AgoraRTC.createCameraVideoTrack();
-    const microphoneTrack = await AgoraRTC.createMicrophoneAudioTrack();
+    localTracks.videoTrack = cameraTrack = await AgoraRTC.createCameraVideoTrack();
+    localTracks.audioTrack = await AgoraRTC.createMicrophoneAudioTrack();
     
     // Publish and play
-    await client.publish(cameraTrack);
-    await client.publish(microphoneTrack);
+    await client.publish(localTracks.videoTrack);
+    await client.publish(localTracks.audioTrack);
     cameraTrack.play("local-player");
 });
 
-client.on("user-published", async (user, mediaType) => {
-    // Initiate the subscription
+async function publishUser(user, mediaType) {
+    console.log("subscribing");
+
     await client.subscribe(user, mediaType);
-    personCount += 1;
+    console.log("Subscription occurring");
   
-    // If the subscribed track is an audio track
     if (mediaType === "audio") {
       const audioTrack = user.audioTrack;
-      // Play the audio
       audioTrack.play();
     } else {
-      const videoTrack = user.videoTrack;
-      // Play the video
-      videoTrack.play("remote-players");
+        const videoTrack = user.videoTrack;
+        const uid = user.id;
         const remotePlayer=$(`
-            <div id="player-${personCount}" class="player"></div>
+            <div class="grid-item">
+                <div id="player-${uid}" class="player"></div>
+            </div>
         `)
-        $("#remote-player").append(remotePlayer);
-        user.videoTrack.play(`player-${personCount}`);
+        $("#video-players").append(remotePlayer);
+        videoTrack.play(`player-${uid}`);
     }
+};
+
+var screenClient;
+var screenTrack;
+
+// Screen Share
+$("#screen").click(async function (e) {
+    e.preventDefault();
+        if (!sharing_screen) {
+        screenClient = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
+        await screenClient.join("fd08b9bee9554c789692cd2477b977ea", $("#room").val(), null);
+
+        screenTrack = await AgoraRTC.createScreenVideoTrack({
+            encoderConfig: "1080p_1",
+        }, "enable")
+        $("#screen").text("Stop Sharing");
+        await screenClient.publish(screenTrack);
+    } else {
+        $("#screen").text("Share Screen");
+        await screenClient.unpublish(screenTrack);
     }
+    sharing_screen = !sharing_screen;
 });
 
-client.on("user-left", async => {
+async function leaveUser() {
     await client.unsubscribe(user, "video");
     await client.unsubscribe(user, "audio");
+    $(`#player-${uid}`).remove();
+};
+
+$("#leave").click(async function () {
+    console.log("am leaving");
+    $("#mic-btn").attr("disabled", true);
+    $("#vid-btn").attr("disabled", true);
+    $("#leave").attr("disabled", true);
+    $("#join").attr("disabled", false);
+    $("#screen").attr("disabled", true);
+    $("#video-players").html(`<div id="local-player" class="grid-item-1" class="player"></div>`);
+    await client.unpublish();
+    await client.leave();
 });
 
 
+$("#mic").click(async function () {
+    if (!mic_muted) {
+        await localTracks.audioTrack.setEnabled(false);
+        $("#mic").text("Unmute Audio");
+    } else {
+        await localTracks.audioTrack.setEnabled(true);
+        $("#mic").text("Mute Audio");
+    }
+    mic_muted = !mic_muted;
+});
 
+$("#vid").click(async function () {
+    if (!vid_muted) {
+        await localTracks.videoTrack.setEnabled(false);
+        $("#vid").text("Unmute Video");
+    } else {
+        await localTracks.videoTrack.setEnabled(true);
+        $("#vid").text("Mute Video");
+    }
+    vid_muted = !vid_muted;
+});
 /*
 await client.leave();
 await client.unpublish();
